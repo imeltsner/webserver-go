51fd00e6ea978d732ad283d2a3c72522dcc17183
isaac meltsner <itmeltsner@dons.usfca.edu>
2024-07-11 22:17:34 -0700

    added get handlers

--- chirps.go
+++ chirps.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"encoding/json"
+	"log"
 	"net/http"
+	"strconv"
 	"strings"
 
 	"github.com/hashicorp/go-set/v2"
@@ -33,6 +35,35 @@ func (cfg *apiConfig) createChirpHandler(w http.ResponseWriter, r *http.Request)
 	}
 
 	respondWithJSON(w, 201, chirp)
+}
+
+func (cfg *apiConfig) getChirpsHandler(w http.ResponseWriter, r *http.Request) {
+	data, err := cfg.db.GetChirps()
+	if err != nil {
+		respondWithError(w, 500, "error gettings chirps")
+		return
+	}
+
+	respondWithJSON(w, 200, data)
+}
+
+func (cfg *apiConfig) getChirpHandler(w http.ResponseWriter, r *http.Request) {
+	idString := r.PathValue("chirpID")
+	chirpID, err := strconv.ParseInt(idString, 10, 64)
+	if err != nil {
+		log.Printf("Error parsing ID %s", err)
+		respondWithError(w, 500, "error parsing ID")
+		return
+	}
+
+	data, err := cfg.db.GetChirp(int(chirpID))
+	if err != nil {
+		log.Printf("Chirp not found %s", err)
+		respondWithError(w, 404, "chirp not found")
+		return
+	}
+
+	respondWithJSON(w, 200, data)
 }
 
 func removeBadWords(words string, badWords set.Set[string]) string {
